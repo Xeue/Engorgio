284c3a04c33647c756ea5d13058dc73f58f14047
sam <sam@chilton.tv>
2025-09-01 17:29:58 +0100

    Added instructions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # grow
 
+Install bunJS
+
 To install dependencies:
 
 ```bash
@@ -10,6 +12,16 @@ To run:
 
 ```bash
 bun run 
+```
+
+To run as service:
+
+Edit the grow.service file to the correct paths (replace USER with your user account)
+Copy it to /etc/systemd/system
+Run:
+```bash
+sudo systemctl enable grow
+sudo systemctl start grow
 ```
 
 This project was created using `bun init` in bun v1.2.21. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
--- server.ts
+++ server.ts
@@ -7,35 +7,23 @@ const __data = path.join(homedir(), 'grow')
 
 const Logs = new _Logs(true, 'grow', __data, 'D', false)
 const Shell = new _Shell(Logs, 'GROW', 'D')
-
-const ports = {
-    'MatchA': '8026',
-    'MatchB': '8263',
-    'MatchC': '8369',
-    'MatchD': '8692',
-    'MatchE': '9040',
-    'MatchF': '9128'
-}
+const publicIP = '10.102.49.11'
 
 Logs.log(`Starting streams`)
 
-doStream('MatchA', 1)
-doStream('MatchB', 1)
-doStream('MatchC', 1)
-// doStream('MatchD')
-// doStream('MatchE')
-// doStream('MatchF')
-
+doStream('MatchA', 1, 8026)
+doStream('MatchB', 1, 8263)
+doStream('MatchC', 1, 8369)
 
 Logs.log(`Started streams`)
 
 
-function doStream(match: string, counter: number) {
+function doStream(match: string, counter: number, port: number) {
     Logs.log(`Starting ${match}`)
     
-    const matchProcess = Shell.process(`sudo ffmpeg -mode listener -i srt://10.102.49.11:${ports[match]} -c:v v210 -vf setfield=tff -r 25 -s 1920x1080 -ac 2 -ar 48000 -y -vcodec libx264 -g 1 -pix_fmt yuv422p10le -r 50000/1000 -x264-params avcintra-class=100:interlaced=0 /mnt/wsl/01_MEDIA/03_LIVE/${match}/${match}_${counter}.mxf`, false)
+    const matchProcess = Shell.process(`sudo ffmpeg -mode listener -i srt://${publicIP}:${port} -c:v v210 -vf setfield=tff -r 25 -s 1920x1080 -ac 2 -ar 48000 -y -vcodec libx264 -g 1 -pix_fmt yuv422p10le -r 50000/1000 -x264-params avcintra-class=100:interlaced=0 /mnt/wsl/01_MEDIA/03_LIVE/${match}/${match}_${counter}.mxf`, false)
     matchProcess.on('exit', ()=>{
         Logs.warn(`${match} has exited`)
-        doStream(match, counter++)
+        doStream(match, counter++, port)
     })
 }
