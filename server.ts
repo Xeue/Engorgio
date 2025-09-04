36df94620612412c3cf8cc9cd4ce583e15ee9fef
sam <sam@chilton.tv>
2025-09-04 10:31:58 +0100

    Made better

--- server.ts
+++ server.ts
@@ -11,6 +11,26 @@ const Shell = new _Shell(Logs, 'GROW', 'D')
 const publicIP = '10.102.49.11'
 const mediaPath = '/mnt/wsl/01_MEDIA/03_LIVE'
 
+const processes: {[key: string]: any} = {};
+
+let exiting = false;
+
+['SIGINT','SIGSTOP', 'SIGTERM'].forEach(signal => {
+    process.on(signal, function () {
+        Logs.log('Stopping process')
+        exiting = true;
+        for (const element of Object.values(processes)) {
+            Logs.warn(`Killing process`)
+            element.kill()
+        }
+        Logs.log('Exiting in 3 second')
+        setTimeout(()=>{
+            process.exit()
+        }, 3*1000)
+    });
+})
+
+
 Logs.log(`Starting streams`)
 
 doStream('MatchA', 1, 8026)
@@ -21,11 +41,14 @@ Logs.log(`Started streams`)
 
 
 function doStream(match: string, counter: number, port: number) {
-    Logs.log(`Starting ${match}`)
+    Logs.log(`Starting ${match} with counter: ${counter}`)
     
     const matchProcess = Shell.process(`sudo ffmpeg -mode listener -i srt://${publicIP}:${port} -c:v v210 -vf setfield=tff -r 25 -s 1920x1080 -ac 2 -ar 48000 -y -vcodec libx264 -g 1 -pix_fmt yuv422p10le -r 50000/1000 -x264-params avcintra-class=100:interlaced=0 ${mediaPath}/${match}/${match}_${counter}.mxf`, false)
+    processes[match] = matchProcess;
     matchProcess.on('exit', ()=>{
+        processes[match] = undefined;
         Logs.warn(`${match} has exited`)
-        doStream(match, counter++, port)
+        if (exiting) return;
+        doStream(match, counter+1, port)
     })
 }
